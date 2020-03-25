5867671f627d95a103d779984a53bcb92440d5ba
Paul Morie <pmorie@redhat.com>
2020-03-25 15:13:40 -0400

    Initial build setup

---
+++ hack/lib/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is meant to be the entrypoint for OpenShift Bash scripts to import all of the support
+# libraries at once in order to make Bash script preambles as minimal as possible. This script recur-
+# sively `source`s *.sh files in this directory tree. As such, no files should be `source`ed outside
+# of this script to ensure that we do not attempt to overwrite read-only variables.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+API_GROUP_VERSIONS="\
+clusters/v1"
+
+API_PACKAGES="\
+github.com/open-cluster-management/api/clusters/v1"
---
+++ hack/update-deepcopy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
+
+verify="${VERIFY:-}"
+
+GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
+  github.com/open-cluster-management/api/generated \
+  github.com/open-cluster-management/api \
+  "clusters:v1" \
+  --go-header-file ${SCRIPT_ROOT}/hack/empty.txt \
+  ${verify}
+
