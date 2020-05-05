60831ca5380ed49a42d2c3c698718886b2962c63
Salvatore Dario Minonne <salvatore-dario.minonne@amadeus.com>
2020-05-05 11:29:10 +0200

    to fix trivia in version checking

--- vendor/github.com/openshift/build-machinery-go/make/lib/golang.mk
+++ vendor/github.com/openshift/build-machinery-go/make/lib/golang.mk
@@ -18,7 +18,7 @@ GOLINT ?=golint
 
 go_version :=$(shell $(GO) version | sed -E -e 's/.*go([0-9]+.[0-9]+.[0-9]+).*/\1/')
 GO_REQUIRED_MIN_VERSION ?=1.13.4
-ifneq "$(GO_REQUIRED_MIN_VERSION)" ""
+ifneq "$(GO_REQUIRED_MIN_VERSION)" "$(go_version)"
 $(call require_minimal_version,$(GO),GO_REQUIRED_MIN_VERSION,$(go_version))
 endif
 
