a657d2a4bad1c6d682df638a2ee246fe43ed5abd
xuezhaojun <zxue@redhat.com>
2022-02-08 11:29:11 +0000

    Fix webhook configuration to pointer
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- operator/v1/zz_generated.swagger_doc_generated.go
+++ operator/v1/zz_generated.swagger_doc_generated.go
@@ -38,7 +38,6 @@ var map_ClusterManagerSpec = map[string]string{
 	"placementImagePullSpec":    "PlacementImagePullSpec represents the desired image configuration of placement controller/webhook installed on hub.",
 	"nodePlacement":             "NodePlacement enables explicit control over the scheduling of the deployed pods.",
 	"deployOption":              "DeployOption contains the options of deploying a cluster-manager Default mode is used if DeployOption is not set.",
-	"detached":                  "Detached includes configurations we needs for clustermanager in the detached mode.",
 }
 
 func (ClusterManagerSpec) SwaggerDoc() map[string]string {
@@ -58,8 +57,9 @@ func (ClusterManagerStatus) SwaggerDoc() map[string]string {
 }
 
 var map_DeployOption = map[string]string{
-	"":     "DeployOption describes the deploy options for cluster-manager or klusterlet",
-	"mode": "Mode can be Default or Detached. For cluster-manager:\n  - In Default mode, the Hub is installed as a whole and all parts of Hub are deployed in the same cluster.\n  - In Detached mode, only crd and configurations are installed on one cluster(defined as hub-cluster). Controllers run in another cluster (defined as management-cluster) and connect to the hub with the kubeconfig in secret of \"external-hub-kubeconfig\"(a kubeconfig of hub-cluster with cluster-admin permission).\nFor klusterlet:\n  - In Default mode, all klusterlet related resources are deployed on the managed cluster.\n  - In Detached mode, only crd and configurations are installed on the spoke/managed cluster. Controllers run in another cluster (defined as management-cluster) and connect to the mangaged cluster with the kubeconfig in secret of \"external-managed-kubeconfig\"(a kubeconfig of managed-cluster with cluster-admin permission).\nThe purpose of Detached mode is to give it more flexibility, for example we can install a hub on a cluster with no worker nodes, meanwhile running all deployments on another more powerful cluster. And we can also register a managed cluster to the hub that has some firewall rules preventing access from the managed cluster.\n\nNote: Do not modify the Mode field once it's applied.",
+	"":         "DeployOption describes the deploy options for cluster-manager or klusterlet",
+	"mode":     "Mode can be Default or Detached. For cluster-manager:\n  - In Default mode, the Hub is installed as a whole and all parts of Hub are deployed in the same cluster.\n  - In Detached mode, only crd and configurations are installed on one cluster(defined as hub-cluster). Controllers run in another cluster (defined as management-cluster) and connect to the hub with the kubeconfig in secret of \"external-hub-kubeconfig\"(a kubeconfig of hub-cluster with cluster-admin permission).\nFor klusterlet:\n  - In Default mode, all klusterlet related resources are deployed on the managed cluster.\n  - In Detached mode, only crd and configurations are installed on the spoke/managed cluster. Controllers run in another cluster (defined as management-cluster) and connect to the mangaged cluster with the kubeconfig in secret of \"external-managed-kubeconfig\"(a kubeconfig of managed-cluster with cluster-admin permission).\nThe purpose of Detached mode is to give it more flexibility, for example we can install a hub on a cluster with no worker nodes, meanwhile running all deployments on another more powerful cluster. And we can also register a managed cluster to the hub that has some firewall rules preventing access from the managed cluster.\n\nNote: Do not modify the Mode field once it's applied.",
+	"detached": "Detached includes configurations we needs for clustermanager in the detached mode.",
 }
 
 func (DeployOption) SwaggerDoc() map[string]string {
