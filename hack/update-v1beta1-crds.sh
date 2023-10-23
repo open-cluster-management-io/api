6ec504f69bfb79bac5c8e3cc6e1f9ea744713404
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2023-10-22 23:42:40 -0400

    :bug: ARM architecture fixes (#292)
    * Skip cleanup if client wasn't instantiated
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
    
    * Set arch to amd64 for controller-gen install
    
    The binary is only built for `amd64`
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
    
    * Upgrade `yq`
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- hack/update-v1beta1-crds.sh
+++ hack/update-v1beta1-crds.sh
@@ -4,7 +4,7 @@ CONTROLLER_GEN_VERSION=v0.6.0
 CONTROLLER_GEN=_output/bin/controller-gen-$CONTROLLER_GEN_VERSION
 controller_gen_dir=$(dirname $CONTROLLER_GEN)
 GOHOSTOS=$(go env GOHOSTOS)
-GOHOSTARCH=$(go env GOHOSTARCH)
+GOHOSTARCH=amd64
 
 if ! which $CONTROLLER_GEN > /dev/null;
   then echo "Installing controller-gen ...";
@@ -20,4 +20,3 @@ $CONTROLLER_GEN schemapatch:manifests="./crdsv1beta1" paths="./work/v1" 'output:
 # Because the ClusterClaim CRD is stable now, we can comment out the following line.
 # Uncomment to generate ClusterClaim CRD when cluster/v1alpha1/types.go is changed
 # $CONTROLLER_GEN schemapatch:manifests="./crdsv1beta1" paths="./cluster/v1alpha1" 'output:dir="./crdsv1beta1"'
-
