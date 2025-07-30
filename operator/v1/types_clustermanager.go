5e3423e7794a7b1e680fa3e31140b8472c514701
Wei Liu <liuweixa@redhat.com>
2025-07-30 20:29:47 +0800

    add grpc registration driver in klusterlet (#387)
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- operator/v1/types_clustermanager.go
+++ operator/v1/types_clustermanager.go
@@ -150,7 +150,7 @@ type GRPCConfig struct {
 	// +optional
 	EndpointExposure *GRPCEndpointExposure `json:"endpointExposure,omitempty"`
 
-	// AutoApprovedIdentities represent a list of approved arn patterns
+	// AutoApprovedIdentities represent a list of approved users
 	// +optional
 	AutoApprovedIdentities []string `json:"autoApprovedIdentities,omitempty"`
 }
