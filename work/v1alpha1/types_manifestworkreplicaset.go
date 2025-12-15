ee922fbb996c731ffbcd17852a87c5efc3e6cf89
Jian Qiu <jqiu@redhat.com>
2025-12-15 11:28:11 +0800

    Move mwrs related labels/finalizers to api repo (#409)
    Signed-off-by: Jian Qiu <jqiu@redhat.com>

--- work/v1alpha1/types_manifestworkreplicaset.go
+++ work/v1alpha1/types_manifestworkreplicaset.go
@@ -24,9 +24,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:object:root=true
@@ -78,9 +75,6 @@ type ManifestWorkReplicaSetSpec struct {
 
 // ManifestWorkReplicaSetStatus defines the observed state of ManifestWorkReplicaSet
 type ManifestWorkReplicaSetStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
 	// Conditions contains the different condition statuses for distrbution of ManifestWork resources
 	// Valid condition types are:
 	// 1. AppliedManifestWorks represents ManifestWorks have been distributed as per placement All, Partial, None, Problem
@@ -186,6 +180,20 @@ const (
 	//
 	// Reason: AsExpected, NotAsExpected or Processing
 	ManifestWorkReplicaSetConditionManifestworkApplied string = "ManifestworkApplied"
+)
+
+const (
+	// ManifestWorkReplicaSetControllerNameLabelKey is the label key on manifestwork to ref to the ManifestWorkReplicaSet
+	// that owns this manifestwork
+	ManifestWorkReplicaSetControllerNameLabelKey = "work.open-cluster-management.io/manifestworkreplicaset"
+
+	// ManifestWorkReplicaSetPlacementNameLabelKey is the label key on manifestwork to ref to the Placement that selects
+	// the managedCluster on the manifestWorkReplicaSet's PlacementRef.
+	ManifestWorkReplicaSetPlacementNameLabelKey = "work.open-cluster-management.io/placementname"
+
+	// ManifestWorkReplicaSetFinalizer is the name of the finalizer added to ManifestWorkReplicaSet. It is used to ensure
+	// related ManifestWorks are deleted.
+	ManifestWorkReplicaSetFinalizer = "work.open-cluster-management.io/manifest-work-cleanup"
 )
 
 // CascadeDeletionPolicy decides the manifestWorkReplicaSet is deleted before/after the related manifestWorks are gone.
