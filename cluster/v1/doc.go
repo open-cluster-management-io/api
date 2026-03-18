d5f5e86b5c8ec08eea260cf5f15568dfaa51ec30
xuezhaojun <zxue@redhat.com>
2026-03-18 13:25:59 +0800

    Add +k8s:openapi-gen=true tag to cluster/v1
    Add the +k8s:openapi-gen=true build tag to cluster/v1/doc.go,
    consistent with cluster/v1beta2 which already has this tag.
    
    This tag is required by downstream consumers that run openapi-gen
    (from k8s.io/code-generator) to generate OpenAPI schema definitions
    for types in this package. Starting with Kubernetes 1.35 (k8s.io
    v0.35.x), the openapi-gen tool became stricter and skips packages
    that lack this tag, even when explicitly passed via --extra-pkgs.
    
    Without this tag, downstream aggregated API servers that serve
    ManagedCluster resources fail at startup with:
      "cannot find model definition for
       open-cluster-management.io/api/cluster/v1.ManagedCluster"
    
    Signed-off-by: xuezhao <zxue@redhat.com>
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- cluster/v1/doc.go
+++ cluster/v1/doc.go
@@ -2,6 +2,7 @@
 // Package v1 contains API Schema definitions for the cluster v1 API group
 
 // +k8s:deepcopy-gen=package,register
+// +k8s:openapi-gen=true
 // +kubebuilder:validation:Optional
 // +groupName=cluster.open-cluster-management.io
 package v1
